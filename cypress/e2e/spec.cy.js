398b3fde32eb15f9ea65739e4bcf99b053712b5f
adam-labbane <adamlabbane.dev@gmail.com>
2025-06-23 11:48:17 +0200

    skip test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,57 +6,57 @@ describe('Workflow utilisateur complet', () => {
     cy.visit('http://localhost:3000');
   });
 
-  it.skip('crée un admin, un user, affiche la liste, voit les détails puis le supprime', () => {
-    cy.fillUserForm({
-      firstname: 'Admin',
-      lastname: 'Test',
-      email: adminEmail,
-      birthdate: '1980-01-01',
-      city: 'Nice',
-      zipcode: '06000',
-      password: 'adminpass',
-      isAdmin: true
-    });
+  // it.skip('crée un admin, un user, affiche la liste, voit les détails puis le supprime', () => {
+  //   cy.fillUserForm({
+  //     firstname: 'Admin',
+  //     lastname: 'Test',
+  //     email: adminEmail,
+  //     birthdate: '1980-01-01',
+  //     city: 'Nice',
+  //     zipcode: '06000',
+  //     password: 'adminpass',
+  //     isAdmin: true
+  //   });
 
-    cy.fillUserForm({
-      firstname: 'User',
-      lastname: 'Simple',
-      email: userEmail,
-      birthdate: '1995-05-05',
-      city: 'Lyon',
-      zipcode: '69000',
-      password: 'userpass',
-      isAdmin: false
-    });
+  //   cy.fillUserForm({
+  //     firstname: 'User',
+  //     lastname: 'Simple',
+  //     email: userEmail,
+  //     birthdate: '1995-05-05',
+  //     city: 'Lyon',
+  //     zipcode: '69000',
+  //     password: 'userpass',
+  //     isAdmin: false
+  //   });
 
-    cy.get('ul').should('exist');
-    cy.get('ul li').contains(userEmail).should('exist');
+  //   cy.get('ul').should('exist');
+  //   cy.get('ul li').contains(userEmail).should('exist');
 
-    cy.get('input[placeholder="Ton email admin"]').clear().type(adminEmail);
-    cy.get('ul li').contains(userEmail).closest('li').within(() => {
-      cy.contains('Voir').click();
-    });
-    cy.contains('Détails de l’utilisateur');
-    cy.get('pre').should('exist');
+  //   cy.get('input[placeholder="Ton email admin"]').clear().type(adminEmail);
+  //   cy.get('ul li').contains(userEmail).closest('li').within(() => {
+  //     cy.contains('Voir').click();
+  //   });
+  //   cy.contains('Détails de l’utilisateur');
+  //   cy.get('pre').should('exist');
 
-    cy.get('input[placeholder="Ton email admin"]').clear().type(adminEmail);
-    cy.get('ul li').contains(userEmail).closest('li').within(() => {
-      cy.contains('Supprimer').click();
-    });
-    cy.on('window:alert', txt => {
-      expect(txt).to.contain('Utilisateur');
-      expect(txt).to.contain('supprimé');
-    });
-  });
+  //   cy.get('input[placeholder="Ton email admin"]').clear().type(adminEmail);
+  //   cy.get('ul li').contains(userEmail).closest('li').within(() => {
+  //     cy.contains('Supprimer').click();
+  //   });
+  //   cy.on('window:alert', txt => {
+  //     expect(txt).to.contain('Utilisateur');
+  //     expect(txt).to.contain('supprimé');
+  //   });
+  // });
 
-  it('empêche la soumission si le prénom est manquant', () => {
-    cy.get('input[name="lastname"]').type('Test');
-    cy.get('button[type="submit"]').click();
+  // it('empêche la soumission si le prénom est manquant', () => {
+  //   cy.get('input[name="lastname"]').type('Test');
+  //   cy.get('button[type="submit"]').click();
 
-    cy.on('window:alert', () => {
-      throw new Error('Le formulaire ne doit pas se soumettre sans prénom');
-    });
-  });
+  //   cy.on('window:alert', () => {
+  //     throw new Error('Le formulaire ne doit pas se soumettre sans prénom');
+  //   });
+  // });
 
   it('empêche la création avec un email déjà utilisé', () => {
     const email = `dup${Date.now()}@test.com`;
