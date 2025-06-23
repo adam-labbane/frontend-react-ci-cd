963ae1afd3f45669c372ec410f395fa0b0ee721f
adam-labbane <adamlabbane.dev@gmail.com>
2025-06-23 11:31:43 +0200

    ajout url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL
+  baseURL: "https://backend-fast-api-sigma.vercel.app"
 });
 
 export default API;
