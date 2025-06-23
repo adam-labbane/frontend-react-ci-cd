2f510f71f7f6e0692b5a111eb7ff232364875dd5
adam-labbane <adamlabbane.dev@gmail.com>
2025-06-23 12:08:35 +0200

    modif yml

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-fast-api-sigma.vercel.app"
+  baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:3000"
 });
 
 export default API;
